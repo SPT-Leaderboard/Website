32e14988fa8ad01fba56b3b52b0b825374e7ce59
John Doe <hattrix@yandex.ru>
2026-02-07 15:44:34 +0500

    Remove unused function

--- js/app-utils.js
+++ js/app-utils.js
@@ -381,30 +381,6 @@ function capitalize(str, locale = 'en-EN') {
     return str[0].toLocaleUpperCase(locale) + str.slice(1).toLocaleLowerCase(locale);
 }
 
-// Отправка JSON данных
-async function postData(url, data) {
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token // если нужна авторизация
-            },
-            body: JSON.stringify(data) // преобразуем объект в JSON
-        });
-        
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.error('Error:', error);
-        throw error;
-    }
-}
-
 async function getCustomProfileSettings(profileId) {
     try {
         const response = await fetch(profileAppearencePath, {
